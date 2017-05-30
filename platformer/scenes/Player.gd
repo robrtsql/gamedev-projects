fe2cbc5404acbe1bf073d4f2443ab8f31c0b72cc
Robert Sigler <sigle023@umn.edu>
2017-05-30 07:35:19 -0500

    Add basic platformer physics
    -Gravity is exerted on player
    -Player does not fall through floor

--- platformer/scenes/Player.gd
+++ platformer/scenes/Player.gd
@@ -1,8 +1,13 @@
 extends KinematicBody2D
 
-var WALK_SPEED = 2
+var WALK_SPEED = 100
+var TERMINAL_VELOCITY = 200
+var UP_ANGLE = (PI/-2.0)
+var MAX_UP_ANGLE = UP_ANGLE + (PI/4)
+var MIN_UP_ANGLE = UP_ANGLE - (PI/4)
 var velocity = Vector2()
 var animator
+var grounded = false
 
 func _ready():
 	animator = get_node("Sprite/AnimationPlayer")
@@ -15,16 +20,27 @@ func _fixed_process(delta):
 		velocity.x =  WALK_SPEED
 	else:
 		velocity.x = 0
-	if (Input.is_action_pressed("ui_up")):
-		velocity.y = -WALK_SPEED
-	elif (Input.is_action_pressed("ui_down")):
-		velocity.y =  WALK_SPEED
-	else:
-		velocity.y = 0
+	if (not grounded):
+		velocity.y += 300 * delta
+	if (velocity.y > TERMINAL_VELOCITY):
+		velocity.y = TERMINAL_VELOCITY
 	
 	if (velocity.x != 0 or velocity.y != 0):
 		animator.set_anim("Walk")
-		move(velocity)
+		var motion = velocity * delta
+		move(motion)
+		
+		grounded = false
+		if (is_colliding()):
+			var n = get_collision_normal()
+			var normal_angle = atan(n.y / n.x)
+			if (normal_angle >= MIN_UP_ANGLE 
+				and normal_angle <= MAX_UP_ANGLE):
+				velocity.y = 0
+				grounded = true
+			motion = n.slide(motion)
+			velocity = n.slide(velocity)
+			move(motion)
 	else:
 		animator.set_anim("Idle")
 	pass
