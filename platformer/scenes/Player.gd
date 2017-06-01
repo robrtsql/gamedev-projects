496d1926fd275028ab9d99181d24ef8407369192
robrtsql <robrtsql@gmail.com>
2017-06-01 07:37:10 -0500

    Apply gravity even when player is on ground
    This way, we are always 'colliding' with the ground and therefore always
    have a normal that we can look at to determine whether or not we are
    standing on the ground or not.

--- platformer/scenes/Player.gd
+++ platformer/scenes/Player.gd
@@ -1,5 +1,9 @@
 extends KinematicBody2D
 
+var state = 1
+var GROUNDED = 0
+var MIDAIR = 1
+
 var WALK_SPEED = 100
 var TERMINAL_VELOCITY = 200
 var UP_ANGLE = (PI/-2.0)
@@ -13,34 +17,52 @@ func _ready():
 	animator = get_node("Sprite/AnimationPlayer")
 	set_fixed_process(true)
 
-func _fixed_process(delta):
+func _move_horizontally():
 	if (Input.is_action_pressed("ui_left")):
 		velocity.x = -WALK_SPEED
 	elif (Input.is_action_pressed("ui_right")):
 		velocity.x =  WALK_SPEED
 	else:
 		velocity.x = 0
-	if (not grounded):
-		velocity.y += 300 * delta
+
+func _apply_gravity(delta):
+	velocity.y += 300 * delta
 	if (velocity.y > TERMINAL_VELOCITY):
 		velocity.y = TERMINAL_VELOCITY
-	
-	if (velocity.x != 0 or velocity.y != 0):
-		animator.set_anim("Walk")
-		var motion = velocity * delta
+
+func _move_with(velocity, delta):
+	if (velocity.x == 0 and velocity.y == 0):
+		return
+	var motion = velocity * delta
+	move(motion)
+	grounded = false
+	if (is_colliding()):
+		var n = get_collision_normal()
+		var normal_angle = atan(n.y / n.x)
+		if (normal_angle >= MIN_UP_ANGLE
+			and normal_angle <= MAX_UP_ANGLE):
+			velocity.y = 0
+			grounded = true
+		motion = n.slide(motion)
+		velocity = n.slide(velocity)
 		move(motion)
-		
-		grounded = false
-		if (is_colliding()):
-			var n = get_collision_normal()
-			var normal_angle = atan(n.y / n.x)
-			if (normal_angle >= MIN_UP_ANGLE 
-				and normal_angle <= MAX_UP_ANGLE):
-				velocity.y = 0
-				grounded = true
-			motion = n.slide(motion)
-			velocity = n.slide(velocity)
-			move(motion)
+	if (grounded):
+		state = GROUNDED
 	else:
-		animator.set_anim("Idle")
+		state = MIDAIR
+
+func _fixed_process(delta):
+	if (state == GROUNDED):
+		_move_horizontally()
+		_apply_gravity(delta)
+		if (velocity.x != 0):
+			animator.set_anim("Walk")
+		else:
+			animator.set_anim("Idle")
+		_move_with(velocity, delta)
+	elif (state == MIDAIR):
+		_move_horizontally()
+		_apply_gravity(delta)
+		animator.set_anim("Fall")
+		_move_with(velocity, delta)
 	pass
