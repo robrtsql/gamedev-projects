0139407c6619a47fef2ec274a01accad2a16aefe
robrtsql <robrtsql@gmail.com>
2017-07-20 07:39:09 -0500

    WIP drawing of tiled map

--- platformer/Tiled.gd
+++ platformer/Tiled.gd
@@ -8,6 +8,11 @@ func _ready():
 	file.open('tiled/testlevel2/testlevel2.json', file.READ)
 	test_level_json.parse_json(file.get_as_text())
 	file.close()
+	
+	var tileset_image = load('textures/spritesheet.png')
+	if !(tileset_image extends Texture):
+		print("was not a texture!! oh snap!!")
+	
 	for layer in test_level_json['layers']:
 		if layer['name'] == 'Object Layer 1':
 			for object in layer['objects']:
@@ -18,12 +23,22 @@ func _ready():
 			var data = layer['data']
 			var size = data.size()
 			for i in range(0, size):
-				var x = i % layer_width
-				var y = i - (x * layer_width)
+				var x = i % int(layer_width)
+				var y = floor(i / layer_width)
 				print(x, ',', y)
-	for child in get_children():
-		print(child)
+				add_child(_create_sprite_object(x, y, test_level_json['tilewidth'], test_level_json['tileheight'], data[i], tileset_image))
+	#for child in get_children():
+	#	print(child)
 	pass
+
+func _create_sprite_object(x, y, tile_width, tile_height, tile_index, tileset_image):
+	var sprite = Sprite.new()
+	sprite.set_pos(Vector2(x * tile_width, y * tile_height))
+	sprite.set_texture(tileset_image)
+	sprite.set_vframes(30)
+	sprite.set_hframes(30)
+	sprite.set_frame(tile_index)
+	return sprite
 
 func _create_collision_object(object):
 	var x = object['x']
