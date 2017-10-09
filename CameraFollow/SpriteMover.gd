9943764664d24ac12a14b8bf082b1deef559b252
robrtsql <robrtsql@gmail.com>
2017-10-09 07:26:03 -0500

    Add minimal project for camera problem

---
+++ CameraFollow/SpriteMover.gd
@@ -0,0 +1,16 @@
+extends Sprite
+
+export(bool) var physics = true
+
+func _ready():
+	set_process(true)
+	set_physics_process(true)
+	pass
+
+func _process(delta):
+	if not physics:
+		translate(Vector2(randi()%1000+1, 0))
+
+func _physics_process(delta):
+	if physics:
+		translate(Vector2(randi()%1000+1, 0))
